da61f8d4cf5f9ae81aa27b837d2fee0112d569ce
JainaSaiShanker <49595298+JainaSaiShanker@users.noreply.github.com>
2023-11-29 01:53:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const storyRoutes = require('./routes/storyRoutes');
+const userRoutes = require('./routes/userRoutes');
 const moongoose = require('mongoose');
 const mainRoutes = require('./routes/mainRoutes');
 const methodOverride = require('method-override');
@@ -13,7 +14,7 @@ let host = 'localhost';
 let url = 'mongodb://localhost:27017/NBAD';
 
 // Connect to MongoDB
-moongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+moongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, })
     .then(() => {
         console.log('Connected to MongoDB');
         app.listen(port, host, () => {
@@ -33,22 +34,20 @@ app.use(methodOverride('_method'));
 
 app.use('/', mainRoutes); // Use mainRoutes for general site navigation
 app.use('/stories', storyRoutes); // Use storyRoutes for handling connections
-
+app.use('/users', userRoutes); // Use userRoutes for handling user login
 
 app.use((req, res, next) => {
-    let err = new Error('The server cannot locate ' + req.url);
+    let err = new Error('The Server cannot locate '+ req.url);
     err.status = 404;
     next(err);
 });
 
 app.use((err, req, res, next) => {
-    if (!err.status) {
-        err.status = 404;
-        err.message = 'Something went wrong';
+    console.log(err.stack);
+    if(!err.status){
+        err.status = 500;
+        err.message = ("Internal Server Error")
     }
-
-    res.status(err.status);
-    res.render('error', { error: err });
+     res.status(err.status);
+     res.render('error', {error:err});
 });
-
-
